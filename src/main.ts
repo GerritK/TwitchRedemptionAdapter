ed87e69de0c17ea16da9a862371dcccf8874c66b
Gerrit Kaul <mail@gerritk.net>
2021-02-03 16:07:32 +0100

    adds config and mqtt

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { PubSubClient } from 'twitch-pubsub-client';
 import { ApiClient, StaticAuthProvider } from 'twitch';
+import * as mqtt from 'mqtt';
+import * as fs from 'fs';
 
 async function startup() {
+    const config = await getConfig();
+    console.log(config);
+
+    // MQTT
+    const mqttClient = mqtt.connect(config.mqtt.host);
+    mqttClient.on('connect', () => {
+        console.log('MQTT CONNECT');
+        mqttClient.subscribe('#');
+    });
+    mqttClient.on('message', (topic, message) => {
+        console.log(topic, message.toString());
+    });
+    mqttClient.on('error', (err) => console.error(err));
+
+    // Twitch
     const authProvider = new StaticAuthProvider(
-        'gp762nuuoqcoxypju8c569th9wz7q5',
-        'ronlwe5iesbyrsaipcnywq5k10qmn5',
+        config.twitch.clientId,
+        config.twitch.accessToken,
         ['channel:read:redemptions'],
     );
     const apiClient = new ApiClient({
@@ -14,7 +31,26 @@ async function startup() {
     const userId = await client.registerUserListener(apiClient);
 
     const redemptionListener = await client.onRedemption(userId, (msg) => {
-        console.log(msg.rewardName);
+        const reward = config.rewardMap.find((r) => r.id === msg.rewardId);
+        mqttClient.publish(reward.topic, msg.redemptionDate.getTime().toString(10));
+    });
+}
+
+async function getConfig(): Promise<any> {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./config.json', (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            try {
+                const result = JSON.parse(data.toString());
+                resolve(result);
+            } catch (err) {
+                reject(err);
+            }
+        });
     });
 }
 
